cdf1daa0921eb0bc007dd28ad44f903c1cc6c190
Connor Myers <connor-j-myers@protonmail.com>
2022-10-11 15:29:58 +1000

    updated version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='connor-j-myers@protonmail.com',
     packages=['codecrawler'],
     install_requires=['requests', 'javalang'],
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='Grabs Java code and documentation from GitHub repositories'
 )
